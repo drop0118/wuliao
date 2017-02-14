4e63358e47232b5bc9bd32dfcc0f47f26f033360
EdisonSR <61781882@qq.com>
2017-02-14 15:01:32 +0800

    Dongtu

--- resources/views/image_list.blade.php
+++ resources/views/image_list.blade.php
@@ -10,17 +10,19 @@
 	<div class='imageList'>
 	<?php foreach ($images as $key => $image) {?>
 		<div class="row">
-			<p><?php echo $image->description;?></p>
-			<?php if(){?>
-				<img  src="<?php echo $image->mw_url?>">
+			<?php echo $image->description ? '<p>'.$image->description.'</p>':'';?>
+			<?php if(substr($image->mw_url, -3,3)=='jpg'){?>
+				<img  style="max-width: 100%; max-height: 700px;" src="<?php echo $image->mw_url;?>">
 			<?php }else{?>
+				<img src="<?php echo $image->mw_url;?>" org_src="<?php echo $image->normal_url;?>"  style="max-width: 100%; max-height: 700px;">
+				<div class="gif-mask" >PLAY</div>
 			<?php }?>
 		</div>
 	<?php }?>
 	</div>
 	<div class="page-Btn">
-		<div class="previous-btn"> &lt; 前滚翻</div>
-		<div class="next-btn">后空翻 &gt;</div>
+		<a href='<?php echo url('images').'?'.http_build_query(['page'=>$current_page-1]);?>' class="previous-btn"> &lt; 前滚翻</a>
+		<a href='<?php echo url('images').'?'.http_build_query(['page'=>$current_page+1]);?>' class="next-btn">后空翻 &gt;</a>
 	</div>
 </body>
 </html>
@@ -28,10 +30,22 @@
 <script src="//cdn.bootcss.com/fastclick/1.0.6/fastclick.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
+    $('.imageList').find('.gif-mask').each(function () {
+       	$(this).parent().css('position', 'relative');
+        var img =$(this).prev();
+        var position = img.position();
+       	$(this).css({
+            'height': img.height(),
+            'width': img.width(),
+            'line-height': img.height() + 'px',
+            'left': position.left,
+            'top': position.top
+        });
+    });
 	FastClick.attach(document.body);
 	$('.imageList img').each(function () {
-		$(this).css('max-width', '100%');
-		$(this).css('max-height', '700px').click(function () {
+		// $(this).css('max-width', '100%');
+		$(this).click(function () {
 			if ($(this).css('max-height') == 'none') {
 				var max_width = $(this).parent().parent().width();
 				$(this).css('max-width', max_width);
@@ -41,12 +55,16 @@ $(document).ready(function(){
 			}
 		});
 	});
-	$(document).on('click','.next-btn',function(){
-		window.location.href = '<?php echo url('images').'?'.http_build_query(['page'=>$current_page+1]);?>';
-	});
 
-	$(document).on('click','.previous-btn',function(){
-		window.location.href = '<?php echo url('images').'?'.http_build_query(['page'=>$current_page-1]);?>';
-	});
+	$(document).on('click', '.gif-mask',function(){
+	    var parent = $(this).parent();
+	    var img = $(this).prev('img');
+	    var org_src = img.attr('org_src');
+	    if (org_src != '') {
+	        img.attr('src', org_src);
+	        img.removeAttr('org_src');
+	        $(this).remove();
+	    }
+	})
 })
 </script>
