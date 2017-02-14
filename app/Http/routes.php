8928164e5dfd782ec89ff8245522382c47a276d2
RoyWang <yuelongnet@163.com>
2017-02-14 16:24:32 +0800

    小修改

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('test', ['uses' => 'TestController@crawler']);
-Route::get('images', ['uses' => 'ImagesController@getImageList']);
+Route::get('/', ['uses' => 'ImagesController@getImageList']);
